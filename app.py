d1e3b318dfa4b562786c70238a327c0b29b2d029
gusthallquoz-cmd <gusthallquoz@gmail.com>
2026-05-08 21:10:19 +0200

    c

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ section[data-testid="stSidebar"]  { display: none; }
 /* Base */
 html, body, .stApp {
     font-size: 16px;
-    background-color: #87cefa;
+    background-color: #f0f6fc;
     font-family: 'Segoe UI', sans-serif;
     color: #1a3a5c;
 }
